152aa68632854ae029d4de26b4ae136bd7a863ce
Stefano Giacoia <s.giacoia@gmail.com>
2023-07-13 11:43:31 +0200

    Time: 15 ms (26.22%), Space: 46.5 MB (91.15%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    Map<Integer,Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        if (map.containsKey(nums[i])) {
+                int valore = map.get(nums[i]);
+                valore++;
+                map.put(nums[i],valore);
+            } else {
+                map.put(nums[i],1);
+            }
+        }
+        Integer chiaveMassima = null;
+        int valoreMassimo = Integer.MIN_VALUE;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > valoreMassimo) {
+                chiaveMassima = entry.getKey();
+                valoreMassimo = entry.getValue();
+            }
+        } 
+        return chiaveMassima;
+    }
+}
