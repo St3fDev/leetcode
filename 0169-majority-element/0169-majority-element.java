7c7dd04cc9118262cdb32afb056043c858cc8bd1
Stefano Giacoia <s.giacoia@gmail.com>
2023-07-13 11:46:32 +0200

    Update 0169-majority-element.java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -21,3 +21,13 @@ class Solution {
         return chiaveMassima;
     }
 }
+
+/*
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+    }
+}
+*/
