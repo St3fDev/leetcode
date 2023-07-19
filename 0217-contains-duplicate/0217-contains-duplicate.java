d3b6484dfc0952e70cb5f3badbaa463c8460f2ad
Stefano Giacoia <s.giacoia@gmail.com>
2023-07-19 23:50:41 +0200

    Time: 21 ms (33.28%), Space: 58.1 MB (7.39%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -9,3 +9,15 @@ class Solution {
         return false;
     }
 }
+
+/*
+public boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Integer> seen = new HashMap<>();
+        for (int num : nums) {
+            if (seen.containsKey(num) && seen.get(num) >= 1)
+                return true;
+            seen.put(num, seen.getOrDefault(num, 0) + 1);
+        }
+        return false;
+    }
+*/
