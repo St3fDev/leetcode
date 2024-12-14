9926aadb8d0854e651f6fa512e16035b7743ec4e
Stefano Giacoia <s.giacoia@gmail.com>
2024-12-14 17:03:57 +0100

    Time: 9 ms (87.82%), Space: 20 MB (60.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -12,4 +12,13 @@ class Solution:
         return results
         
         
-                
+# better solution
+'''
+anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
+'''
